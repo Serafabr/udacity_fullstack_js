e0fe473c202a92f614ac91f5c095406b5bfd8215
Pedro Henrique Serafim <serafaobr@gmail.com>
2023-04-18 20:31:48 -0300

    Create gitignore and delete some folders

--- project_01_image_processing/dist/utils/createThumbnailFilename.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createThumbnailFilename = void 0;
-const createThumbnailFilename = (filename, fileExt, width = 'auto', height = 'auto') => {
-    return `${filename.split(".")[0]}` +
-        `-${width}-${height}${fileExt}`;
-};
-exports.createThumbnailFilename = createThumbnailFilename;
